947311b677ccd8ac38d9f393b1cd163d7e4fdef5
Talha <talhashahidch@gmail.com>
2023-06-14 17:14:49 +0500

    new commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -51,7 +52,27 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
+
+        <div className="flex justify-between items-cente gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log in
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
