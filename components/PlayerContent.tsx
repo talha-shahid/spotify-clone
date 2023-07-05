96e00eacfed656b6530630d140ce4306c060d264
Talha <talhashahidch@gmail.com>
2023-07-05 18:51:20 +0500

    done

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -4,8 +4,12 @@ import { Song } from "@/types";
 import LikeButton from "./LikeButton";
 import MediaItem from "./MediaItem";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
+import useSound from "use-sound";
 
 interface PlayerContentProps {
   song: Song;
@@ -13,8 +17,84 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
+  const player = usePlayer();
   const [isPlaying, setIsPlaying] = useState(true);
+  const [volume, setVolume] = useState(1);
+
   const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  //onPlayNext
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+
+    const nextSong = player.ids[currentIndex + 1];
+
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong);
+  };
+
+  //onPlayPrevious
+  const onPlayPrevious = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+
+    const previousSong = player.ids[currentIndex - 1];
+
+    if (!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+
+    player.setId(previousSong);
+  };
+
+  //useSound hook
+  const [play, { pause, sound }] = useSound(songUrl, {
+    volume: volume,
+    onplay: () => setIsPlaying(true),
+    onend: () => {
+      setIsPlaying(false);
+      onPlayNext();
+    },
+    onpause: () => setIsPlaying(false),
+    format: ["mp3"],
+  });
+
+  //useEffect
+  useEffect(() => {
+    sound?.play();
+
+    return () => {
+      sound?.upload();
+    };
+  }, [sound]);
+
+  //handlePlay
+  const handlePlay = () => {
+    if (!isPlaying) {
+      play();
+    } else {
+      pause();
+    }
+  };
+
+  //toggleMute
+  const toggleMute = () => {
+    if (volume === 0) {
+      setVolume(1);
+    } else {
+      setVolume(0);
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="flex w-full justify-start">
@@ -26,7 +106,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
 
       <div className="flex md:hidden col-auto w-full justify-end items-center">
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon size={30} className="text-black" />
@@ -35,21 +115,32 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
 
       <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
         <AiFillStepBackward
-          onClick={() => {}}
+          onClick={onPlayPrevious}
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon size={30} className="text-black" />
         </div>
         <AiFillStepForward
-          onClick={() => {}}
+          onClick={onPlayNext}
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
+      </div>
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon
+            onClick={toggleMute}
+            className="cursor-pointer"
+            size={34}
+          />
+          <Slider value={volume} onChange={(value) => setVolume(value)} />
+        </div>
       </div>
     </div>
   );
